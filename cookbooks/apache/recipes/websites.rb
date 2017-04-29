804886660d9d17f9a9aeb1404ba58eefba5869b1
Shawn Doyle <sdoyle@releaseteam.com>
2017-04-29 19:46:59 +0000

    playing with environments

--- cookbooks/apache/recipes/websites.rb
+++ cookbooks/apache/recipes/websites.rb
@@ -1,4 +1,11 @@
 file 'default www' do
 	path 'var/www/html/index.html'
-	content 'Hello World'
+	content 'Hello World! version 2.0'
 end
+
+#webnodes = search('node', 'role:web')
+
+#webnodes.each do |node|
+#	puts node
+#end
+
